4d48de8656c289be3512de57b3f66bc036619180
Quentin L <cdql@hotmail.fr>
2023-04-25 10:27:47 +0200

    Adding some comments + Minor changes

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const fns = require("date-fns");
 
 class Technician {
-    /* Create a Technician (name / preference) */
+    /** Create Technician objects
+     * @param name: Technician's name
+     * @type name: string
+     * @param preference: Technician's preference concerning when he/she doesn't want to work during the week
+     * @type preference: string (Day of the week with capital letter and in english)
+     * */
     constructor(name, preference = "Any") {
         this.name = name;
         this.preference = preference;
@@ -9,18 +14,31 @@ class Technician {
         membersT.push(this);
     }
 
-    /* Allow to edit a Technician name */
+    /** Allow to edit name of a Technician object
+     * @param newName: New name that will be updated
+     * @type newName: string
+     * */
     editName(newName) {
         this.name = newName;
     }
 
-    /* Update working time by the duration for a technician*/
+    /** Allow to increase workingTime of a Technician object
+     * @param duration: Value of increasing workingTime
+     * @type duration: integer
+     * @return type: Object of Technician Class
+     * */
     addTime(duration) {
         this.workingTime += duration;
         return this;
     }
 
-    /* Take an array full with Technician objects as input and return the Technician with the least workingTime */
+
+    /** Take an array as input and return the Technician with the least workingTime among all
+     * @param array: array full with Technician objects
+     * @type array: array
+     * @return minimum: Technician with the least value in workingTime
+     * @type minimum: Object of Technician class
+     * */
     static minTime(array) {
         let minimum = array[0];
         for (let i = 1; i < array.length; i++) {
@@ -46,14 +64,17 @@ class Days {
         for (let i = 0; i < times.length; i++) {
             do {
                 let tech = Technician.minTime(available);
-                if (tech.preference != fns.format(date, "EEEE")) {
+                if (tech.preference !== fns.format(date, "EEEE")) {
                     break;
                 }
                 else {
                     let index = available.indexOf(tech);
                     available.splice(index, 1);
                 }
-            } while(true)
+            } while(available.length !== 0)
+            if (available.length === 0) {
+                throw new Error("Il est impossible de trouver un arrangement selon les préférences pour cette date : " + this.date);
+            }
             let tech = Technician.minTime(available);
             tech.addTime(times[i]["duration"]);
             let index = available.indexOf(tech);
@@ -69,7 +90,9 @@ class Days {
     }
 }
 
-
+/**
+ *
+ * */
 function schedule(days, times, members) {
     const list = [];
     let date = new Date();
@@ -82,6 +105,9 @@ function schedule(days, times, members) {
     return list;
 }
 
+/**
+ *
+ * */
 function main() {
     let noms = ["Person1", "Person2", "Person3", "Person4", "Person5", "Person6", "Person7", "Person8", "Person9"];
     let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
@@ -90,20 +116,20 @@ function main() {
         {start: '9h', end: '15h', duration: 6},
         {start: '20h', end: '8h', duration: 12}];
     /* Create 10 test tech */
-    for (let i = 0; i < 9; i++) {
-        let temp = "test" + i.toString();
-        temp = new Technician(noms[i], days[i % days.length]);
+    try {
+        for (let i = 0; i < 9; i++) {
+            let temp = "test" + i.toString();
+            // temp = new Technician(noms[i], days[i % days.length]);
+            temp = new Technician(noms[i], "Wednesday");
+        }
+        console.log("----------------------------------------------------------------")
+        console.log(schedule(28, times, membersT));
+        console.log("----------------------------------------------------------------")
+        console.log(membersT)
     }
-    /* console.log(members);
-    console.log("----------------------------------------------------------------")
-    let test = new Days(7,7,8,12,members);
-    console.log(test)
-    console.log("----------------------------------------------------------------")
-    console.log(members) */
-    console.log("----------------------------------------------------------------")
-    console.log(schedule(28, times, membersT));
-    console.log("----------------------------------------------------------------")
-    console.log(membersT)
+    catch (e) {
+        console.error(e)
+    }
 }
 
 const membersT = [];
